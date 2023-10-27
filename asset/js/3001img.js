91e26c9d268e3da42adcb3ff3ec4a287e862e732
pangsusu <love2wind@huayuworld.org>
2023-10-27 09:37:33 +0800

    update

--- asset/js/3001img.js
+++ asset/js/3001img.js
@@ -3,7 +3,7 @@ $.fn.fileinput.defaults.language = 'zh';
 let $smfile = $("#file");
 $smfile.fileinput({
     // theme: 'fas',
-    uploadUrl: BASE_URL + '/3001imgbed',
+    uploadUrl: 'https://img.131213.xyz/3001imgbed',
     allowedFileExtensions: ['jpeg', 'jpg', 'png', 'gif', 'bmp'],
     // allowedFileExtensions: ['jpeg', 'jpg', 'png', 'gif', 'bmp', 'mp4', 'mov', 'avi'],
     overwriteInitial: false,
